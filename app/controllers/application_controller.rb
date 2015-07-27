ccdfeea32d4208a35854582d4f0bda4fefbd7c32
Jamie Barton <jamie@codethisway.com>
2015-07-27 14:12:09 +0100

    Add Controller and view helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+  def current_user
+    @current_user ||= session[:user_id] && User.find(session[:user_id])
+  end
+
+  private
+
+  helper_method :signed_in?
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def authorize
+    unless signed_in?
+      session[:return_to] = request.original_url
+      redirect_to '/auth/twitter', alert: 'You must login to access this page.'
+    end
+  end
 end
